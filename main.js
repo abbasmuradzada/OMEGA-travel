52e87a04b928fc0dcced252b4639547064f81272
abbasmuradzada <abbasam@code.edu.az>
2020-05-31 19:13:29 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function direction_home(){
+    window.location.href="index.html"
+}
+
+function direction_about(){
+    window.location.href="index.html#about"
+}
+
+function direction_services(){
+    window.location.href="index.html#sevices"
+}
+
+function direction_trip(){
+    window.location.href="index.html#trip"
+}
+
+function direction_reviews(){
+    window.location.href="index.html#reviews"
+}
+
+function direction_blog(){
+    window.location.href="index.html#blog"
+}
+
